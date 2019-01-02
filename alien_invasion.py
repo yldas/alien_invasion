004f0d36c6313aea8ac70bcecb8c879fc1948d4e
yldas <misael.valentin@upr.edu>
2019-01-02 01:20:38 -0400

    Added ship image, refactored

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
-import sys
-
 import pygame
 
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -12,19 +12,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # Make a ship.
+    ship = Ship(screen)
+
     # Start the main loop for the game.
     while True:
-
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-
-        # Make the most recently draw screen visible.
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
