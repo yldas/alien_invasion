04f74b1d606f20ade741e6b401d91780b1bf395a
yldas <misael.valentin@upr.edu>
2019-01-02 13:53:04 -0400

    Added left/right movement

--- settings.py
+++ settings.py
@@ -5,5 +5,8 @@ class Settings:
         """ Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 650
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
