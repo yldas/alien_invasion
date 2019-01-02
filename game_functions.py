1312c694f50fc284353bbe90d69f97b49d40ba03
yldas <misael.valentin@upr.edu>
2019-01-02 01:47:41 -0400

    Fixed indentation error

--- game_functions.py
+++ game_functions.py
@@ -12,8 +12,8 @@ def check_events():
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
-            screen.fill(ai_settings.bg_color)
-            ship.blitme()
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
 
-            # Make the most recently draw screen visible.
-            pygame.display.flip()
+    # Make the most recently draw screen visible.
+    pygame.display.flip()
